4092226c9d0cdb5719ee78ff6a9ae3fe062a528f
sfomuseumbot <sfomuseumbot@localhost>
2023-08-17 17:20:48 -0700

    snapshot: compiles again

--- app/reversegeo/reversegeo.go
+++ app/reversegeo/reversegeo.go
@@ -9,13 +9,12 @@ import (
 	"github.com/aaronland/go-jsonl/walk"
 	"github.com/aaronland/gocloud-blob/bucket"
 	"github.com/sfomuseum/go-flags/flagset"
-	"github.com/tidwall/gjson"
 	"github.com/whosonfirst/go-overture/geojsonl"
 	"github.com/whosonfirst/go-whosonfirst-spatial-hierarchy"
-	hierarchy_filter "github.com/whosonfirst/go-whosonfirst-spatial-hierarchy/filter"		
+	hierarchy_filter "github.com/whosonfirst/go-whosonfirst-spatial-hierarchy/filter"
+	_ "github.com/whosonfirst/go-whosonfirst-spatial-sqlite"
 	"github.com/whosonfirst/go-whosonfirst-spatial/database"
 	spatial_filter "github.com/whosonfirst/go-whosonfirst-spatial/filter"
-	_ "github.com/whosonfirst/go-whosonfirst-spatial-sqlite"
 )
 
 func Run(ctx context.Context, logger *log.Logger) error {
@@ -42,34 +41,42 @@ func RunWithFlagSet(ctx context.Context, fs *flag.FlagSet, logger *log.Logger) e
 	defer source_bucket.Close()
 
 	// Set up spatial database
-	
+
 	spatial_db, err := database.NewSpatialDatabase(ctx, spatial_database_uri)
 
 	if err != nil {
 		return fmt.Errorf("Failed to create spatial database, %w", err)
 	}
-	
-	resolver, err := hierarchy.NewPointInPolygonHierarchyResolver(ctx, spatial_db, nil)
+
+	resolver_opts := &hierarchy.PointInPolygonHierarchyResolverOptions{
+		Database: spatial_db,
+	}
+
+	resolver, err := hierarchy.NewPointInPolygonHierarchyResolver(ctx, resolver_opts)
 
 	if err != nil {
 		return fmt.Errorf("Failed to create new PIP resolver, %w", err)
 	}
-	
+
 	inputs := &spatial_filter.SPRInputs{}
-	
+
 	results_cb := hierarchy_filter.FirstButForgivingSPRResultsFunc
 	update_cb := hierarchy.DefaultPointInPolygonHierarchyResolverUpdateCallback()
-		
+
 	// Walk Overture records
 
 	walk_cb := func(ctx context.Context, r *walk.WalkRecord) error {
 
-		new_body, err := resolver.PointInPolygonAndUpdate(ctx, inputs, results_cb, update_cb, r.Body)
+		has_changed, _, err := resolver.PointInPolygonAndUpdate(ctx, inputs, results_cb, update_cb, r.Body)
 
 		if err != nil {
 			return fmt.Errorf("Failed to update record, %w", err)
 		}
-		
+
+		if !has_changed {
+			return nil
+		}
+
 		return nil
 	}
 
